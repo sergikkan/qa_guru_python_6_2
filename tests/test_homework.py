955e3bc8151cf07eb563b72d80e0a510f02ea46c
kans <sergik-199022@mail.ru>
2023-07-21 15:06:20 +0600

    Добавил тест с дз

---
+++ tests/test_homework.py
@@ -0,0 +1,22 @@
+import pytest
+from selene import browser, be, have
+
+
+@pytest.fixture(scope="module")
+def maximize_browser_window():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+
+
+def test_google_search(maximize_browser_window):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests'))
+
+
+def test_no_search_results(maximize_browser_window):
+    browser.open('https://google.com')
+    random_string = 'E81RvIDd2KZrdsYQrwDRLsgbzVUOQy8naI9arDgwiijXiy1iX6'
+    browser.element('[name="q"]').should(be.blank).type(random_string).press_enter()
+    browser.element('[id="topstuff"]').should(have.text(f'По запросу {random_string} ничего не найдено.'))
+    print('При поиске по произвольной строке ничего не находится')
